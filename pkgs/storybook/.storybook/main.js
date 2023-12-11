44b529bcdc6b0df75efb09b657fe389a4575b8ea
Brandon <mauzybwy@gmail.com>
2023-12-11 10:23:28 -0500

    Friendship ended with storybook, now cosmos is my best friend

--- pkgs/storybook/.storybook/main.js
+++
@@ -1,9 +0,0 @@
-const appdir = require.resolve('@org/app');
-
-module.exports = {
-  stories: [
-    appdir + "/../components/**/*.stories.mdx",
-    appdir + "/../components/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
-  addons: ['@storybook/addon-ondevice-controls', '@storybook/addon-ondevice-actions'],
-};
